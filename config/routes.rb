de930437239fa7c64669bdedc57ed48b744231a9
Johnny Nguyen <jnguyen@scruggs.com>
2010-05-19 21:58:14 -0500

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :chatrooms
+  
+  map.root :controller => "chatrooms"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
