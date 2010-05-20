7ec3756d29f7da429210b25672472821b6a96324
Johnny <johnnyn@gmail.com>
2010-05-20 10:20:57 -0500

    fixed form reset

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,12 +3,13 @@ class ChatroomsController < ApplicationController
   def add_line
     data = render_to_string :update do |page|
       page.insert_html :top, 'chat_list', "<li>#{params[:name]}: #{h params[:entry]}</li>"
-      page[:entry].clear
-      page[:entry].focus
     end
 
     Orbited.send_data('hello', data)
-    render :nothing => true
+    render :update do |page|
+      page[:entry].clear
+      page[:entry].focus
+    end
   end
 
 end
